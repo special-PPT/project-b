058d762f5bfa45b2f6964a1a3a70c72e2137fd3a
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2024-01-06 21:53:35 -0800

    refactor: encapsulate applicant status to 1 component

--- src/front-end/src/components/employee/EmployeeHome.tsx
+++ src/front-end/src/components/employee/EmployeeHome.tsx
@@ -1,29 +1,7 @@
-import { Container, Typography } from "@mui/material";
+import ApplicantStatus from "./ApplicantStatus";
 
 export default function EmployeeHome() {
-  const applicationStatus = "Approved";
-  const applicationFeedback = "Your application has been approved, please use other tabs to edit or download documents.";
-
   return (
-    <Container maxWidth="xl">
-      <Typography
-        sx={{
-          fontFamily: "Poppins, sans-serif",
-          fontSize: "h5.fontSize",
-          mt: 2,
-        }}
-        variant="h2"
-      >
-        Application Status: {applicationStatus}
-      </Typography>
-      <Typography
-        sx={{
-          fontFamily: "Poppins, sans-serif",
-          mt: 1,
-        }}
-      >
-        {applicationFeedback}
-      </Typography>
-    </Container>
+    <ApplicantStatus />
   );
 }
