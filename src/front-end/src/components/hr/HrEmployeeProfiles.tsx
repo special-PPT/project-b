9fb46058c734a6e5ef4f8868be1a42c8ac955e71
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2024-01-07 16:20:36 -0800

    build: added search bar (searching) and sorting functionality to HR's employee dashboard

--- src/front-end/src/components/hr/HrEmployeeProfiles.tsx
+++ src/front-end/src/components/hr/HrEmployeeProfiles.tsx
@@ -7,6 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+import TableSearch from "./TableSearch";
 
 interface Column {
   id: "employee_id" | "name" | "ssn" | "work_auth" | "phone" | "email";
@@ -48,52 +49,12 @@ function createData(
 // TODO: get data from API
 const rows = [
   createData(
-    101,
-    "Alice Smith",
-    "123-45-6789",
-    "Yes",
-    "555-0101",
-    "alice@example.com"
-  ),
-  createData(
-    102,
-    "Bob Johnson",
-    "987-65-4321",
-    "No",
-    "555-0102",
-    "bob@example.com"
-  ),
-  createData(
-    103,
-    "Carol Williams",
-    "234-56-7890",
-    "Yes",
-    "555-0103",
-    "carol@example.com"
-  ),
-  createData(
     104,
     "David Brown",
     "890-12-3456",
     "Yes",
     "555-0104",
     "david@example.com"
-  ),
-  createData(
-    105,
-    "Eve Davis",
-    "567-89-0123",
-    "No",
-    "555-0105",
-    "eve@example.com"
-  ),
-  createData(
-    106,
-    "Frank Miller",
-    "345-67-8901",
-    "Yes",
-    "555-0106",
-    "frank@example.com"
   ),
   createData(
     107,
@@ -104,12 +65,12 @@ const rows = [
     "grace@example.com"
   ),
   createData(
-    108,
-    "Henry Wilson",
-    "678-90-1234",
+    102,
+    "Bob Johnson",
+    "987-65-4321",
     "No",
-    "555-0108",
-    "henry@example.com"
+    "555-0102",
+    "bob@example.com"
   ),
   createData(
     109,
@@ -120,14 +81,6 @@ const rows = [
     "ivy@example.com"
   ),
   createData(
-    110,
-    "Jack Davis",
-    "890-12-3456",
-    "No",
-    "555-0110",
-    "jack@example.com"
-  ),
-  createData(
     111,
     "Kathy Brown",
     "901-23-4567",
@@ -136,12 +89,12 @@ const rows = [
     "kathy@example.com"
   ),
   createData(
-    112,
-    "Liam Smith",
-    "012-34-5678",
+    105,
+    "Eve Davis",
+    "567-89-0123",
     "No",
-    "555-0112",
-    "liam@example.com"
+    "555-0105",
+    "eve@example.com"
   ),
   createData(
     113,
@@ -152,12 +105,12 @@ const rows = [
     "mia@example.com"
   ),
   createData(
-    114,
-    "Noah Williams",
-    "234-56-7890",
-    "No",
-    "555-0114",
-    "noah@example.com"
+    101,
+    "Alice Smith",
+    "123-45-6789",
+    "Yes",
+    "555-0101",
+    "alice@example.com"
   ),
   createData(
     115,
@@ -167,11 +120,60 @@ const rows = [
     "555-0115",
     "olivia@example.com"
   ),
+  createData(
+    110,
+    "Jack Davis",
+    "890-12-3456",
+    "No",
+    "555-0110",
+    "jack@example.com"
+  ),
+  createData(
+    103,
+    "Carol Williams",
+    "234-56-7890",
+    "Yes",
+    "555-0103",
+    "carol@example.com"
+  ),
+  createData(
+    106,
+    "Frank Miller",
+    "345-67-8901",
+    "Yes",
+    "555-0106",
+    "frank@example.com"
+  ),
+  createData(
+    108,
+    "Henry Wilson",
+    "678-90-1234",
+    "No",
+    "555-0108",
+    "henry@example.com"
+  ),
+  createData(
+    112,
+    "Liam Smith",
+    "012-34-5678",
+    "No",
+    "555-0112",
+    "liam@example.com"
+  ),
+  createData(
+    114,
+    "Noah Williams",
+    "234-56-7890",
+    "No",
+    "555-0114",
+    "noah@example.com"
+  ),
 ];
 
 export default function HrEmployeeProfiles() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [searchQuery, setSearchQuery] = React.useState('');
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -184,6 +186,15 @@ export default function HrEmployeeProfiles() {
     setPage(0);
   };
 
+  const handleSearchChange = (query: string) => {
+    setSearchQuery(query.toLowerCase());
+  };
+
+  const filteredRows = rows.filter((row) => {
+    // TODO: is name part of input in backend APIs?
+    return row.name.toLowerCase().includes(searchQuery);
+  });
+
   return (
     <Paper
       sx={{
@@ -193,10 +204,12 @@ export default function HrEmployeeProfiles() {
         alignSelf: "center",
         marginTop: "30px",
         borderStyle: "solid",
-        borderWidth: "1px",
-        borderColor: (theme) => theme.palette.primary.dark, 
+        borderWidth: "2px",
+        borderRadius: "12px",
+        borderColor: (theme) => theme.palette.primary.dark,
       }}
     >
+      <TableSearch onSearchChange={handleSearchChange} />
       <TableContainer
         sx={{
           minHeight: "500px",
@@ -222,7 +235,15 @@ export default function HrEmployeeProfiles() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows
+            {filteredRows
+              .sort((a, b) => {
+                // Split the names to get the last names
+                const lastNameA = a.name.split(" ").pop()?.toLowerCase() ?? "";
+                const lastNameB = b.name.split(" ").pop()?.toLowerCase() ?? "";
+
+                // Compare the last names
+                return lastNameA.localeCompare(lastNameB);
+              })
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
