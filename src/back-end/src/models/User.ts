7626d46f82b8b05e774da0b486a5a1e420e3193a
Yanru Chen <47041483+yanruc123@users.noreply.github.com>
2024-01-23 17:23:47 -0800

    fix: email should be unique

--- src/back-end/src/models/User.ts
+++ src/back-end/src/models/User.ts
@@ -15,7 +15,7 @@ export interface IUser extends Document {
 
 const userSchema: Schema = new Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
   isActive: { type: Boolean, default: false },
