9b290c3a18d77a161615b67ceba97c50c494bb4f
Zhengmao Zhang <malker@qq.com>
2024-01-25 11:58:38 -0800

    refeact: put data

--- src/front-end/src/components/onboard/Summary.tsx
+++ src/front-end/src/components/onboard/Summary.tsx
@@ -2,13 +2,20 @@ import { Button, Container, Grid, Paper, Typography } from '@mui/material'
 import React, { useContext } from 'react'
 import { UserContext } from './UserContext';
 import { useCookies } from 'react-cookie';
+import axios from 'axios';
 export default function Summary() {
     // const user = useSelector((state: RootState) => state.user);
     const {userInfo} = useContext(UserContext);
     const [cookies] = useCookies(['userID']);
     const userID = cookies.userID;
-    const handleSumbit = () =>{
-        
+    const handleSumbit = async () =>{
+        await axios.put(`http://localhost:8000/personalInfo/update/${userID}`, userInfo)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
     }
   return (
     <Container>
@@ -75,7 +82,7 @@ export default function Summary() {
         <Grid item xs={12}>
             <Button variant='contained' color='primary'
                 fullWidth
-                
+                onClick={handleSumbit}
             >
                 Submit
             </Button>
